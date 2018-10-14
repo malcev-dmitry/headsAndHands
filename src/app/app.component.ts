49bc816eaf1359e0b62a59f099d8069d1ad1e63a
malcev-dmitry <WWWgeniusnet1993>
2018-10-14 17:48:08 +0300

    fix: добавил файлы с данными и вывел маркеры на карту

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { stations } from '../assets/data/stations.js';
+import { data } from '../assets/data/data.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
   // google maps zoom level
-  zoom = 8;
+  zoom = 12;
 
   // initial center position for the map
-  lat = 51.673858;
-  lng = 7.815982;
+  lat = 59.9434483211;
+  lng = 30.3601813316;
+
+  markers: any;
+
+  ngOnInit() {
+    this.markers = stations;
+    console.log(data);
+  }
 }
