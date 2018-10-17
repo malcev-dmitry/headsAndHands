89f514a59de5277cdfb37ef24d2772dbeb432c5e
malcev-dmitry <WWWgeniusnet1993>
2018-10-17 15:14:43 +0300

    fix: сделал более менее адаптивной боковую панель под мобилы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { stations } from '../assets/data/stations.js';
-import { data } from '../assets/data/data.js';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +8,52 @@ import { data } from '../assets/data/data.js';
 })
 
 export class AppComponent implements OnInit {
-  private icon = {
-    url: require('../assets/icons/metroIcon.svg'),
-    scaledSize: {
-      height: 35,
-      width: 28.5
-    }
-  };
 
-  constructor() {}
+  private icon = [
+    {
+      url: '../assets/icons/group.svg',
+      scaledSize: {
+        height: 48,
+        width: 120
+      }
+    },
+    {
+      url: '../assets/icons/star.svg',
+      scaledSize: {
+        height: 13.3,
+        width: 13.3
+      }
+    },
+    {
+      url: '../assets/icons/page.svg',
+      scaledSize: {
+        height: 13.3,
+        width: 13.3
+      }
+    },
+    {
+      url: '../assets/icons/starHPMD.png',
+      scaledSize: {
+        height: 48,
+        width: 120
+      }
+    },
+    {
+      url: '../assets/icons/pageHPMD.png',
+      scaledSize: {
+        height: 48,
+        width: 120
+      }
+    },
+  ];
 
   markers: any;
 
+  constructor() {}
+
   zoom = 12;
-  lat = 59.9434483211;
-  lng = 30.3601813316;
+  lat = 55.7094361;
+  lng = 37.6546389;
 
   action = true;
   memoryAction = 1;
@@ -32,11 +62,19 @@ export class AppComponent implements OnInit {
     if (this.memoryAction !== numberCity) {
       this.action = !this.action;
       this.memoryAction = numberCity;
+
+      if (numberCity === 1) {
+        this.lat = 55.7094361;
+        this.lng = 37.6546389;
+      }
+      if (numberCity === 2) {
+        this.lat = 59.910123;
+        this.lng = 30.2854809;
+      }
     }
   }
 
   ngOnInit() {
     this.markers = stations;
-    console.log(data);
   }
 }
